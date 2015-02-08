c96020e206e7743962306dc08ce129a8b31f154b
pressla <pressl.alex@imitrix.de>
2015-02-08 18:06:54 +0100

    init

---
+++ app.js
@@ -0,0 +1,39 @@
+
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 80 });
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            listing: true
+        }
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', response: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
